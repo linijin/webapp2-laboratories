7e2d3613a7f7b0489d042283b67f185ee01136db
Lenie Jane Tinagsa <167656306+linijin@users.noreply.github.com>
2024-05-17 16:51:21 +0800

    Add files via upload

---
+++ 02_simple_php_syntax.php
@@ -0,0 +1,46 @@
+<?php
+
+$name = 'Lenie Jane V. Tinagsa';
+$age = 21 ;
+$address = 'Phase 3 Brgy. Payatas Quezon City Metro Manila' ;
+$hobbies = 'Drawing';
+$pet_peeve = 'Alcoholic ';
+
+echo $name , '<br>';
+echo $age, '<br>';
+echo $address, '<br>';
+echo $hobbies, '<br>';
+echo $pet_peeve, '<br><br>';
+
+$name = 'Lenie Jane V. Tinagsa ';
+$age = 21 ;
+$address = ' Phase 3 Brgy. Payatas Quezon City Metro Manila ' ;
+// $hobbies = 'Drawing ';
+// $pet_peeve = 'Alcoholic ';
+
+
+
+define('NAME', 'lenie');
+define('AGE', 21);
+define('ADDRESS','phase 3 brgy. payatas quezon city metro manila');
+define('HOBBIES', 'drawing');
+define('PET_PEEVE', 'alcoholic');
+
+echo NAME . '<br>'; 
+echo AGE, '<br>';
+echo ADDRESS, '<br>';
+echo HOBBIES, '<br>';
+echo PET_PEEVE, '<br>';
+
+echo '<pre>';
+var_dump(NAME, AGE, ADDRESS, HOBBIES, PET_PEEVE) ;
+echo '</pre>' ;
+
+/* I 
+don't 
+like
+the 
+taste 
+of 
+it*/
+
